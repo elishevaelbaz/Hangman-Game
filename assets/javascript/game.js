201d9836628916408c02a278accdef144d3783e6
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-07 18:35:46 -0500

    dashes to show without commas, game pretty much working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,6 +16,8 @@ var guessed = [];
 var word = list[Math.floor(Math.random() * list.length)];
 
 var wordArray = [];
+
+var wordStr = "";
 var wordDisplay = [];
 
 wordArray = word.split("");
@@ -25,17 +27,19 @@ for (var i = 0; i < word.length; i++){
 }
 
 
-console.log(word);
-console.log("word Array: " + wordArray);
-console.log("word Display: " + wordDisplay);
-
+// console.log(word);
+// console.log("word Array: " + wordArray);
+// console.log("word Display: " + wordDisplay);
 
 document.onkeyup = function(event){
 
 	var input = event.key;
 	console.log("letter guessed: " + input);
 	console.log("index of the letter: " + word.indexOf(input))
+
+	//index of the first appearance of the input letterin the word
 	var index = word.indexOf(input);
+
 	// only execute if the key pressed is a letter 
 	//(lowercase or uppercase)
 
@@ -44,18 +48,18 @@ document.onkeyup = function(event){
 		// Im sure there is a better way to do this
 		if (letters.indexOf(input.toLowerCase()) != -1){
 
-		// var input = event.key;
-
-		console.log("you have determined input is a letter");
+			console.log("you have determined input is a letter");
 
 		// if the guessed letter is in the word
-		if (index != -1){
+			if (index != -1){  
 
 			//display where the letter occurs 
 			//(change the wordDisplay array)
 
-			wordDisplay[index] = input;
-			console.log(wordDisplay);
+				wordDisplay[index] = input;
+				wordStr = wordDisplay.join(" ");
+				updateWordDisplay();
+				console.log(wordDisplay);
 
 			//check if the letter appears more than once
 			//start with the element after the first instance
@@ -63,55 +67,60 @@ document.onkeyup = function(event){
 
 			//if it does, display all instances
 
-			for (var i = index + 1; i < word.length; i++) {
-				if (word[i] == input){
-					console.log("guess: " + input + " index: " + i + " letter in array " + word[i])
-					wordDisplay[i] = input;
-					console.log(wordDisplay);
+				for (var i = index + 1; i < word.length; i++) {
+					if (word[i] == input){
+						console.log("guess: " + input + " index: " + i + " letter in array " + word[i])
+						wordDisplay[i] = input;
+						console.log(wordDisplay);
+					}	
 				}
-			}
-
 
 			//if got the whole word (there are no dashes left in wordDisplay)
-			if (wordDisplay.indexOf("_") == -1){
+				if (wordDisplay.indexOf("_") == -1){
 				//display you won message
 				
-				console.log("You Won");
-				wins+=1;
-				updateWins();
+					console.log("You Won");
+					wins+=1;
+					updateWins();
 
 				//show a picture?
 
 				//restart the game
-				resetGame();
-			}
-		}
-
-		else{
-			numGuesses--;
-			console.log("guesses left: " + numGuesses);
-			//add the guessed letter to the guessed array
-			guessed.push(input);
-			//display guessed letters
-			updateGuesses();
-			updateGuessesLeft();
-			console.log(guessed);
-
-			if (numGuesses==0){
-				//display losing message and the answer
-				console.log("You Lost")
-
-				updateLosses();
-
-				resetGame();
-				//stop the counter
+					resetGame();
+				}
 			}
 
-			console.log(word.indexOf(input))
+			//if the letter is not in the word
+			else{
+				numGuesses--;
+				console.log("guesses left: " + numGuesses);
+				//add the guessed letter to the guessed array
+				guessed.push(input);
+				//display guessed letters
+				updateGuesses();
+				updateGuessesLeft();
+				console.log(guessed);
+
+				// if there are no more guesses left
+				if (numGuesses==0){
+					//display losing message and reset the game
+					updateLosses();
+					resetGame();
+			}
+
+			
 		}
 	}
 
 	} //close onkeyevent
+
+
+	//functions
+	function updateWordDisplay(){
+		var wordDisplayHeader = document.getElementById("currentWord");
+		wordDisplayHeader.textContent = (wordStr);
+
+	}
 
 
 	function updateGuesses(){
@@ -132,13 +141,12 @@ document.onkeyup = function(event){
 	}
 
 	function updateLosses(){
-
 		updateMessage("lost");
 	}
 
 	function updateMessage(str){
 		var messageHeader = document.getElementById("message");
-		messageHeader.textContent = ("You " + str + "! The answer was " + word);
+		messageHeader.textContent = ("You " + str + "! The answer was " + word.toUpperCase());
 	}
 
 	function resetGame(){
@@ -157,6 +165,8 @@ document.onkeyup = function(event){
 		for (var i = 0; i < word.length; i++){
 			wordDisplay.push("_");
 		}
+		wordStr = wordDisplay.join(" ");
+		updateWordDisplay();
 
 		console.log("word Array: " + wordArray);
 		console.log("word Display: " + wordDisplay);
