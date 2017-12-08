9e7ba317180101c99184808dbd057776572444bb
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-07 19:49:59 -0500

    game works - no styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,113 +1,105 @@
 
+var game = false;
 var wins = 0;
-var numGuesses = 8;
-
+var numGuesses = 8; 
 var letters = ["a", "b", "c", "d", "e", "f", "g",
-"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-"t", "u", "v", "w", "x", "y", "z"];
+			"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+			"t", "u", "v", "w", "x", "y", "z"];
 
 var list = ["azalea", "buttercup", "carnation", "chrysanthemum",
-"cornflower", "crocus", "daffodil", "dahlia", "daisy", "dandelion",
-"iris", "lavender", "lilac", "lily", "marigold", "orchid",
-"rose", "sunflower", "tulip", "violet", "zinnia"];
+			"cornflower", "crocus", "daffodil", "dahlia", "daisy", "dandelion",
+			"iris", "lavender", "lilac", "lily", "marigold", "orchid",
+			"rose", "sunflower", "tulip", "violet", "zinnia"];
 
 var guessed = [];
-
 var word = list[Math.floor(Math.random() * list.length)];
-
 var wordArray = [];
-
-var wordStr = "";
 var wordDisplay = [];
 
+//array with the letters of the answer
 wordArray = word.split("");
 
+// array of correct # of dashes
 for (var i = 0; i < word.length; i++){
 	wordDisplay.push("_");
 }
 
+// to display the dashes and correctly guessed letters 
+//(without having the commas)
+var wordStr = wordDisplay.join(" ");
 
-// console.log(word);
-// console.log("word Array: " + wordArray);
-// console.log("word Display: " + wordDisplay);
 
+//when key is pressed
 document.onkeyup = function(event){
 
 	var input = event.key;
-	console.log("letter guessed: " + input);
-	console.log("index of the letter: " + word.indexOf(input))
 
-	//index of the first appearance of the input letterin the word
+	//index of the first appearance of the input letter in the word
 	var index = word.indexOf(input);
 
-	// only execute if the key pressed is a letter 
-	//(lowercase or uppercase)
+	//To start a new game
+	if (input == "Enter"){
+		game = true;
+		console.log("Pressed enter");
+		updateWordDisplay();
+		resetGame();
+	}
 
+	// set a flag, only execute the rest of the game if they press enter
+	if (game == true){
 
 		// check if the user input is a letter (if not, nothing will happen)
 		// Im sure there is a better way to do this
+
 		if (letters.indexOf(input.toLowerCase()) != -1){
 
-			console.log("you have determined input is a letter");
-
-		// if the guessed letter is in the word
+			// if the guessed letter is in the word
 			if (index != -1){  
 
-			//display where the letter occurs 
-			//(change the wordDisplay array)
+				//check if the letter appears more than once
+				//start with the element after the first instance
+				//loop through and check if it's there again
 
-				wordDisplay[index] = input;
-				wordStr = wordDisplay.join(" ");
-				updateWordDisplay();
-				console.log(wordDisplay);
+				//if it does, display all instances
 
-			//check if the letter appears more than once
-			//start with the element after the first instance
-			//loop through and check if it's there again
-
-			//if it does, display all instances
-
-				for (var i = index + 1; i < word.length; i++) {
+				for (var i = index; i < word.length; i++) {
 					if (word[i] == input){
-						console.log("guess: " + input + " index: " + i + " letter in array " + word[i])
 						wordDisplay[i] = input;
-						console.log(wordDisplay);
 					}	
 				}
 
-			//if got the whole word (there are no dashes left in wordDisplay)
+				//display where the letter occurs 
+				//(change the wordDisplay array)
+				wordDisplay[index] = input;
+				wordStr = wordDisplay.join(" ");
+				updateWordDisplay();
+
+				//if got the whole word (there are no dashes left in wordDisplay)
 				if (wordDisplay.indexOf("_") == -1){
-				//display you won message
 				
-					console.log("You Won");
 					wins+=1;
 					updateWins();
+					//stop their input from being recorded until they press enter
+					game = false; 					
 
-				//show a picture?
-
-				//restart the game
-					resetGame();
 				}
 			}
-
+			
 			//if the letter is not in the word
 			else{
 				numGuesses--;
-				console.log("guesses left: " + numGuesses);
-				//add the guessed letter to the guessed array
-				guessed.push(input);
-				//display guessed letters
-				updateGuesses();
 				updateGuessesLeft();
-				console.log(guessed);
+				//add the guessed letter to the guessed array and display
+				guessed.push(input);
+				updateGuesses();
 
 				// if there are no more guesses left
 				if (numGuesses==0){
-					//display losing message and reset the game
+					//display losing message and stop the game until they press enter
 					updateLosses();
-					resetGame();
+					game = false;
+				}
 			}
-
 			
 		}
 	}
@@ -119,9 +111,7 @@ document.onkeyup = function(event){
 	function updateWordDisplay(){
 		var wordDisplayHeader = document.getElementById("currentWord");
 		wordDisplayHeader.textContent = (wordStr);
-
 	}
-
 
 	function updateGuesses(){
 		var lettersGuessedHeader = document.getElementById("lettersGuessed");
@@ -136,7 +126,6 @@ document.onkeyup = function(event){
 	function updateWins(){
 		var winHeader = document.getElementById("numWins");
 		winHeader.textContent = wins;
-
 		updateMessage("won");
 	}
 
@@ -146,33 +135,34 @@ document.onkeyup = function(event){
 
 	function updateMessage(str){
 		var messageHeader = document.getElementById("message");
-		messageHeader.textContent = ("You " + str + "! The answer was " + word.toUpperCase());
+		messageHeader.textContent = ("You " + str + "! The answer was " +
+			word.toUpperCase());
 	}
 
 	function resetGame(){
+
+		//reset guessesLeft
 		numGuesses = 8;
+		updateGuessesLeft();
+
+		//clear already guessed letters
+		guessed=[];
+		updateGuesses();
+
+		//choose a new word
 		word = list[Math.floor(Math.random() * list.length)];
 		console.log(wins);
 		console.log("New word: " + word);
 		
-
 		//clear word Display and put correct number of dashes
 		// and put new word in word array
 		wordArray = word.split("");
-
 		wordDisplay=[];
-		console.log("word Display: " + wordDisplay);
+
 		for (var i = 0; i < word.length; i++){
 			wordDisplay.push("_");
 		}
 		wordStr = wordDisplay.join(" ");
 		updateWordDisplay();
-
-		console.log("word Array: " + wordArray);
-		console.log("word Display: " + wordDisplay);
-
-//clear already guessed letters
-guessed=[];
-
 
 }
